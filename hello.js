77743d1a899856d2f48988072d92cea4fef66834
Jorge Alor <romaria85@gmail.com>
2019-12-26 17:10:41 -0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	    $.ajax({
 		            url: "http://52.188.138.178/api/customers/api/customers",
-		            headers: { 'Ocp-Apim-Subscription-Key': '4f6bce226bea4eaf9bf6f2ecb2bb1916;product=unlimited' }
+		            headers: { 'Ocp-Apim-Subscription-Key': '5489c2b18b3d4092bf3768fb36b99cc5;product=unlimited' }
 		        }).then(function(data) {
 				       $('.clientes').append(data[Math.floor(Math.random()*3)]);
 				    });
@@ -10,7 +10,7 @@ $(document).ready(function() {
 $(document).ready(function() {
       $.ajax({
               url: "http://52.188.138.178/products/api/products",
-                      headers: { 'Ocp-Apim-Subscription-Key': '9db32e4fed844af0aac4537ed2f784c8;product=unlimited' }
+                      headers: { 'Ocp-Apim-Subscription-Key': '5489c2b18b3d4092bf3768fb36b99cc5;product=unlimited' }
                           }).then(function(data) {
                                  $('.productos').append(data[Math.floor(Math.random()*3)]);
                                      });
